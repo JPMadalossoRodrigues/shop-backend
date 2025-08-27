b36e08f7d4ef2622517eadd943ae89944b37bff7
Joao Pedro Madalosso Rodrigues <jpmadalossorodrigues@gmail.com>
2025-08-27 19:32:10 -0300

    dtos with pipe validation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,31 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import type { CreateUserDto } from './dtos/createuser.dto';
 import { UserService } from './user.service';
 import { UserEntity } from './interfaces/user.entity';
+import { ReturnUserDto } from './dtos/returnuser.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
   @Post()
-  async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
+  async createUser(
+    @Body(new ValidationPipe()) createUser: CreateUserDto,
+  ): Promise<UserEntity> {
     return this.userService.createUser(createUser);
   }
 
   @Get()
-  async getAllUser(): Promise<UserEntity[]> {
-    return this.userService.getAllUsers();
+  async getAllUser(): Promise<ReturnUserDto[]> {
+    return (await this.userService.getAllUsers()).map(
+      (userEntity) => new ReturnUserDto(userEntity),
+    );
   }
 }
